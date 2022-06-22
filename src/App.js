5136967449dba35cc6d23f359b20b7af898e5113
Tom <yous421@gmail.com>
2022-06-22 21:33:46 +0800

    first tabata revision

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Tabata from './tabata/Tabata';
+
+const App = () => {
+  return (
+    <div>
+      <Tabata></Tabata>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/tabata/Tabata.js
@@ -0,0 +1,46 @@
+import './Tabata.css'
+import react, {useState} from 'react';
+
+const Tabata = () => {
+    const [round, setRound] = useState(0);
+    const [work, setWork] = useState(0);
+    const [rest, setRest] = useState(0);
+
+    const runHandler = () => {
+
+        alert(round + ':' + work + ':' + rest);
+    };
+
+    const roundChangeHandler = (event) =>{
+        setRound(event.target.value);
+    };
+
+    const workChangeHandler = (event) =>{
+        setWork(event.target.value);
+    };
+
+    const restChangeHandler = (event) =>{
+        setRest(event.target.value);
+    };
+
+    return (
+        <div>
+            <div>
+                <label for="round">Round:</label>
+                <input type="number" id="round" onChange={roundChangeHandler} />
+                Times
+            </div>
+            <div>
+                <label for="work">Work(sec):</label>
+                <input type="number" id="work" onChange={workChangeHandler} />
+            </div>
+            <div>
+                <label for="rest">Rest(sec):</label>
+                <input type="number" id="rest" onChange={restChangeHandler} />
+            </div>
+            <button type="button" onClick={runHandler}>run</button>
+        </div>
+    );
+};
+
+export default Tabata;
